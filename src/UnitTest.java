ddcc2fe6bf120679aee8b503ef152fa24322213c
szwnba <szwnba@qq.com>
2015-09-20 22:47:55 +0800

    intit

---
+++ src/UnitTest.java
@@ -0,0 +1,20 @@
+//import org.junit.Test;
+
+
+
+/**
+ * Created by Administrator on 2015/9/20.
+ */
+public class UnitTest {
+
+    private static final SelLogger logger = SelLogger.getLogger(UnitTest.class);
+
+//    @Test
+    public static void main(String args[])
+    {
+        logger.log("Start");
+        System.out.println("Hellow world");
+        logger.log("End");
+
+    }
+}
